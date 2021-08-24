5cb2cff58f8703b567fefdb47c54e947214cb935
Alicia Weenum <weenum.alicia@gmail.com>
2021-08-24 10:20:41 -0400

    first commit

---
+++ db/migrate/20210824141514_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :transactions do |t|
+      t.string :payer
+      t.integer :points
+      t.date :timestamp
+
+      t.timestamps
+    end
+  end
+end
